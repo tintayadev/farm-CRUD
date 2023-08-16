ead7d07d325e26bc46b0ed9a1c46390ca780e672
Paulo Tintaya <pau.tintaya.code@gamil.com>
2023-08-16 18:27:57 -0400

    Middleware added for the frontend app

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from routes.task import task
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+
 app = FastAPI()
 
+print(config('FRONTEND_URL'))
+
+origins = [
+    config('FRONTEND_URL')
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
 @app.get('/')
 def welcome():
     return {'message': 'Welcome to the my FastAPI API'}
