963cc01ed3e54512188140579fe9bb93a665ed4d
Paulo Tintaya <pau.tintaya.code@gamil.com>
2023-08-17 16:00:12 -0400

    Task list added

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import axios from "axios";
 
-function TaskForm() {
+function Homepage() {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        async function fetchTasks() {
+            const res = await axios.get('http://localhost:8000/api/tasks')
+            setTasks(res.data);
+        }
+        fetchTasks();
+
+    }, []);
+
     return (
-        <h1 className='text-3xl font-bold'>HomePage</h1>
+        <>
+            <h1 className='text-3xl font-bold'>HomePage</h1>
+
+            {
+                tasks.map(task => (
+                    <div>
+                        <h2>
+                            {task.title}
+                        </h2>
+                        <p>{task.description}</p>
+                    </div>
+                ))
+            }
+        </>
+        
     )
 }
 
-export default TaskForm
+export default Homepage;
